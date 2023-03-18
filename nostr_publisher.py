fcf869950a4ae8ab848d6542461baaab5385cdfe
Joel Klabo <joelklabo@gmail.com>
2023-03-18 09:25:17 +0000

    Remove Commented Out Code

--- nostr_publisher.py
+++ nostr_publisher.py
@@ -28,14 +28,4 @@ class NostrPublisher:
 		)
 		self.private_key.sign_event(dm)
 		self.relay_manager.publish_event(dm)
-
-#publisher = NostrPublisher(["wss://nostr.klabo.blog"], "45e237f0a302a7f5835137716341d5bd9019a86342ed7bd5d57c6e4fde98a47d")
-#event = Event("Hello Nostr")
-#publisher.publish_event(event)
-#time.sleep(1.25)
-#dm = EncryptedDirectMessage(
-#  recipient_pubkey="2f4fa408d85b962d1fe717daae148a4c98424ab2e10c7dd11927e101ed3257b2",
-#  cleartext_content="Secret message!"
-#)
-#publisher.publish_event(dm)
-#time.sleep(1.25)
+		
